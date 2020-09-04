63360b61d8bde67df5316fcb2b931e5d19149b97
Lastname, Firstname <email@abc.com>
2020-09-04 23:19:34 +0200

    react spa wellness

--- src/Contentful.js
+++ src/Contentful.js
@@ -2,6 +2,6 @@ import { createClient } from 'contentful';
 
 export default createClient({
 
-     space: "0iav0leutg8c",
-    accessToken: "PZ_4niEjURoP0u-ayx0AdTqiMiWWF-MGHjmuSc0sdGA"
+     space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
